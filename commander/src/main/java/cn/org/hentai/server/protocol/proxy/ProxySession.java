f79f5e2d4bfc3ea215a47c3d5736042f82bc51f0
matrixy <matrixy@me.com>
2018-04-03 16:09:38 +0800

    删除测试时的sleep

--- commander/src/main/java/cn/org/hentai/server/protocol/proxy/ProxySession.java
+++ commander/src/main/java/cn/org/hentai/server/protocol/proxy/ProxySession.java
@@ -97,7 +97,6 @@ public class ProxySession extends SocketSession
                 decryptAndTransfer(hostIS, clientOS, hostBufLength);
             }
             if (hostBufLength + clientBufLength == 0) Thread.sleep(1);
-            sleep(10);
         }
     }
 
